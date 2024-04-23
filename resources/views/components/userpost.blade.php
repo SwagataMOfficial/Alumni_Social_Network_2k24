01f3c69a40d013f31721920bf336fbf1b0772a9b
SwagataMOfficial <swagatamukherjee365@gmail.com>
2024-04-23 13:47:40 +0530

    added carousel post

--- resources/views/components/userpost.blade.php
+++ resources/views/components/userpost.blade.php
@@ -37,9 +37,8 @@
         </a>
 
         @php
+            // generating date
             use Carbon\Carbon;
-            // Given date
-            // $givenDate = Carbon::createFromDate(2024, 4, 20); // Example: May 1, 2024
             $givenDate = Carbon::createFromDate($posts['created_at']);
 
             // Current date
@@ -61,40 +60,23 @@
 
         {{-- checking for carousel post and normal post  --}}
         @if (stripos($posts['attachment'], '||') != false)
+
+            @php
+                $imgArr = explode('||', $posts['attachment']);
+            @endphp
+
             {{-- carousel --}}
             <div id="indicators-carousel" class="relative w-full mt-4" data-carousel="static">
                 <!-- Carousel wrapper -->
                 <div class="relative h-56 overflow-hidden rounded-lg md:h-80">
                     <!-- Item 1 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                        <img src="storage\2115230100\uploads\1713812029_0.jpg"
-                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="...">
-                    </div>
-                    <!-- Item 2 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="storage\2115230100\uploads\1713814700_0.jpg"
-                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="...">
-                    </div>
-                    <!-- Item 3 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="storage\2115230100\uploads\1713814718_0.jpg"
-                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="...">
-                    </div>
-                    <!-- Item 4 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="storage\2115230100\uploads\1713814737_0.png"
-                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="...">
-                    </div>
-                    <!-- Item 5 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="storage\2115230100\verification_document\verify_doc.jpg"
-                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            alt="...">
-                    </div>
+                    @foreach ($imgArr as $index => $img)
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                            <img src="{{ asset('/storage/' . $img) }}"
+                                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                                alt="...">
+                        </div>
+                    @endforeach
                 </div>
                 <!-- Slider controls -->
                 <button type="button"
@@ -104,8 +86,8 @@
                         class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
                         <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="M5 1 1 5l4 4" />
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M5 1 1 5l4 4" />
                         </svg>
                         <span class="sr-only">Previous</span>
                     </span>
@@ -117,8 +99,8 @@
                         class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
                         <svg class="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2" d="m1 9 4-4-4-4" />
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
                         </svg>
                         <span class="sr-only">Next</span>
                     </span>
@@ -126,9 +108,8 @@
             </div>
         @else
             {{-- not carousel --}}
-            
+            <img src="{{ asset('/storage/' . $posts['attachment']) }}" alt="Posted Picture" class="mt-4 rounded-lg">
         @endif
-        <img src="{{ asset('/storage/' . $posts['attachment']) }}" alt="Posted Picture" class="mt-4 rounded-lg">
     </div>
 
     <!-- Options -->
