78ecc500e7dfaf483b6e008e74a711e147e26ea8
Sk Md Altab <sk.md.altab987@gmail.com>
2024-05-06 00:08:55 +0530

    mail system in admin by altab

---
+++ resources/views/mails/account_banned.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Banned</title>
+</head>
+<body>
+    <p>Hello {{ $user->name }},</p>
+
+    <p>We regret to inform you that your account has been permanently banned from AlumniJunction.</p>
+    
+    <p>Thanks,<br>
+    AlumniJunction Team</p>
+</body>
+</html>
---
+++ resources/views/mails/support_reply.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Support Reply</title>
+</head>
+<body>
+    <h2>My Query:</h2>
+    <p>{{ $query }}</p>
+    <h3>Reply from AlumniJunction:</h3>
+    <p>{{ $reply }}</p>
+</body>
+</html>
