b15a92b61fb474e8c37230ce87835ed8466e42f2
Sk Md Altab <sk.md.altab987@gmail.com>
2024-05-11 10:44:52 +0530

    user management change

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,32 +14,32 @@ class UserSeeder extends Seeder {
     public function run(): void {
         $faker = Faker::create();
 
-        User::create([
-            'student_id' => '2115230110',
-            'name' => 'Swagata Mukherjee',
-            'email' => 'attendancesystem24x66@gmail.com',
-            'password' => bcrypt('attendance'),
-            'graduation_year' => '2024',
-            'degree' => 'BCA',
-            'profile_picture' => "default/avatar.jpg",
-            'cover_picture' => 'default/cover.png',
-            'verification_document' => 'v_doc.img',
-            'remember_token' => md5('2115230110' . 'attendancesystem24x66@gmail.com')
-        ]);
+        // User::create([
+        //     'student_id' => '2115230110',
+        //     'name' => 'Swagata Mukherjee',
+        //     'email' => 'attendancesystem24x66@gmail.com',
+        //     'password' => bcrypt('attendance'),
+        //     'graduation_year' => '2024',
+        //     'degree' => 'BCA',
+        //     'profile_picture' => "default/avatar.jpg",
+        //     'cover_picture' => 'default/cover.png',
+        //     'verification_document' => 'v_doc.img',
+        //     'remember_token' => md5('2115230110' . 'attendancesystem24x66@gmail.com')
+        // ]);
 
-        for ($i = 1; $i <= 19; $i++) {
-            $id = '152' . $faker->numberBetween(100, 999);
+        for ($i = 1; $i <= 5; $i++) {
+            $id = '15201221' . $faker->numberBetween(100, 999);
             User::create([
                 'student_id' => $id,
                 'name' => $faker->name,
-                'email' => 'attendancesystem24x' . $i . '@gmail.com',
-                'password' => bcrypt('attendance'),
-                'graduation_year' => '2024',
+                'email' => 'alumni20x' . $i . '@gmail.com',
+                'password' => bcrypt('123456'),
+                'graduation_year' => '2020',
                 'degree' => 'BCA',
                 'profile_picture' => "default/avatar.jpg",
                 'cover_picture' => 'default/cover.png',
-                'verification_document' => 'v_doc.img',
-                'remember_token' => md5($id . 'attendancesystem24x'. $i .'@gmail.com')
+                'verification_document' => 'default/v_doc.jpg',
+                'remember_token' => md5($id . 'alumni20x'. $i .'@gmail.com')
             ]);
         }
 
